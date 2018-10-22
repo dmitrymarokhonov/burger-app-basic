f90dcf08fc4e05155427dfb416fa96d402d1f3e4
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-10-23 01:53:50 +0300

    Axios added, first post request to FireBase db

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -73,7 +74,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("you conitnue!");
+    // alert("you conitnue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Dmitry Marokhonov",
+        address: {
+          street: "Drevlyanka 24 1",
+          zipcode: "187007",
+          country: "Karelian Republic, Russia"
+        },
+        email: "dmitry.marokhonov@gmail.com"
+      },
+      deliveryMethod: "fastest"
+    };
+    axios
+      .post("orders.json", order)
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
   };
 
   render() {
