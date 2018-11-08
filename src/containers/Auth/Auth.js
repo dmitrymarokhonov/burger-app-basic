d46b9d11180336e5ff6455a13743902d17543ec8
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-11-09 01:29:34 +0200

    314 9:30

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,103 @@
 import React, { Component } from "react";
 
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+import styles from "./Auth.module.scss";
+
 class Auth extends Component {
-  state = {};
+  state = {
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Mail Address"
+        },
+        value: "",
+        validation: {
+          required: true,
+          isMail: true
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "Password"
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
+      }
+    }
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  }
   render() {
+    const formElementsArray = [];
+    for (const key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key]
+      });
+    }
+
+    const form = formElementsArray.map(formElement => {
+      return (
+        <Input
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+          value={formElement.config.value}
+          invalid={!formElement.config.valid}
+          touched={formElement.config.touched}
+          shouldValidate={formElement.config.validation}
+          changed={event => this.inputChnagedHandler(event, formElement.id)}
+        />
+      );
+    });
+
     return (
-      <div>
-        <form />
+      <div className={styles.Auth}>
+        <form>
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
+        </form>
       </div>
     );
   }
